d92530f6e21ce08644dad8008a29e03aca584133
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-10 22:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const textInput = document.querySelector('#text')
+  const delayInput = document.querySelector('#delay')
+  const button = document.querySelector('#btn')
+  const output = document.querySelector('#output')
+
+  const text = textInput?.value
+  const delay = parseInt(delayInput?.value, 10)
+
+  button.addEventListener('click', async (e) => {
+    e.preventDefault()
+
+    if (isNaN(delay)) return
+
+    await new Promise((resolve) => setTimeout(resolve, delay))
+
+    output.innerHTML = `<span>${text}</span>`
+  })
+})
